2910049eb31e59cbe71fd2c0af4102d426bdea5a
Tj Houston <tj@tjhouston.com>
2022-09-26 16:01:52 -0400

    Create loadCTF.sh

---
+++ loadCTF.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+# Make Directories
+mkdir ./enum
+mkdir ./exploits
+mkdir ./notes
+mkdir ./proof
+
+# Download Enumeration Tools
+cd enum
+git clone https://github.com/21y4d/nmapAutomator
+git clone https://github.com/diego-treitos/linux-smart-enumeration
