613b65fc914bba84c88ec8537db075259c02ab0b
Tj Houston <tj@tjhouston.com>
2022-09-27 10:41:52 -0400

    Update loadCTF.sh

--- loadCTF.sh
+++ loadCTF.sh
@@ -17,3 +17,5 @@ cd enum
 git clone https://github.com/21y4d/nmapAutomator
 # https://github.com/diego-treitos/linux-smart-enumeration 
 git clone https://github.com/diego-treitos/linux-smart-enumeration
+# Download Linpeas
+curl -L https://github.com/carlospolop/PEASS-ng/releases/latest/download/linpeas.sh
