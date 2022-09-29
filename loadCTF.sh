0f13e54db92cb87e7a5bb834599cf7c0f5e3b516
Tj Houston <tj@tjhouston.com>
2022-09-29 13:32:58 -0400

    Update loadCTF.sh

--- loadCTF.sh
+++ loadCTF.sh
@@ -5,8 +5,30 @@ mkdir ./exploits
 mkdir ./notes
 mkdir ./proof
 
+
+
+
+
+seclist="$1"
+unrock="$2"
+
+
 # Download Sec Lists
+if [ $seclist = "true" ]; then  
 sudo apt install seclists
+echo "seclists installed"
+else
+echo "seclists not installed"
+fi 
+
+if [ $unrock = "true" ]; 
+then 
+gunzip /usr/share/wordlists/rockyou.txt.gz
+echo "rock you unzipped"
+else
+echo "rockyou not unzipped"
+fi
+
 
 # Download GoBuster
 sudo apt install gobuster
@@ -18,4 +40,5 @@ git clone https://github.com/21y4d/nmapAutomator
 # https://github.com/diego-treitos/linux-smart-enumeration 
 git clone https://github.com/diego-treitos/linux-smart-enumeration
 # Download Linpeas
-git clone https://github.com/carlospolop/PEASS-ng
+# git clone https://github.com/carlospolop/PEASS-ng
+curl -L https://github.com/carlospolop/PEASS-ng/releases/latest/download/linpeas.sh
