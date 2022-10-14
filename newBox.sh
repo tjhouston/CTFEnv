8a65e07aad6b2289ce3f405a5caa7168bdc6c06f
Tj Houston <tj@tjhouston.com>
2022-10-14 18:19:00 -0400

    Create newBox.sh
    Create a workspace with the folders and tools that I use on every box.

---
+++ newBox.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+# Script to create documentation and workspace for CTF boxes.
+# To do: 
+# Export env vars to use in MSF and other Tools
+# Figure out how to integrate nmap automater 
+
+# Enter RHOST IP Address:
+
+read -p "Enter RHOST IP Address: " RHOST
+
+read -p "Enter LHOST IP Address: " LHOST
+
+read -p "Enter Name of Box / Room: " boxName
+
+mkdir $RHOST
+
+touch $boxName_methodology.md
+
+#Make directory for enumeration
+cd $RHOST
+mkdir 01_Enumeration
+cd 01_Enumeration
+mkdir Tools
+# https://github.com/21y4d/nmapAutomator
+git clone https://github.com/21y4d/nmapAutomator
+mkdir Results
+cd ..
+mkdir 02_Exploits
+
+cd ..
+
+
+echo "*** Workspace Setup for $RHOST - '$boxName' is now complete on $LHOST ***"
